8f8fb88cb186534b5b7dfee156bcfd01938f6c32
noumankhatib <zestfulnouman@gmail.com>
2024-12-12 00:02:28 +0530

    added latest changes related to react

--- App.js
+++ App.js
@@ -1,15 +1,19 @@
+
+import React from "react";
+import ReactDOM from "react-dom/client";
 const parent = React.createElement(
   "div",
   { id: "parent" },
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am an h1 tag"),
-    React.createElement("h2", {}, "I am an h2 tag"),
+    React.createElement("h1", {}, "This is namaste react"),
+    React.createElement("h2", {}, "related to nodejs tag"),
   ])
 );
-const heading = React.createElement(
-  "h1",
-  { id: "heading", xyz: "abc" },
-  "Hello World from React!"
-);
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading", xyz: "abc" },
+//   "Hello World from React!"
+// );
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(parent);
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # react
+
+
+# parcel
+- dev build
+- local servere
+- HMR = HOT Module replacement
+- File Watching Algorithms - written in c++
+- Caching - faster build
+- Image Optimization
+- Bundling
+- Compress
+- Consistency hashing
+- code spliting
+- Differential Bundling - support older browser
+- Https Support
+- tree shaking - remove unwanted code
